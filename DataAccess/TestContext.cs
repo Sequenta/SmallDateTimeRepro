e3fedb4da30fc3368a0f1d0fdf1267b0675ac4b1
Mikhail <mivanov@idscan.net>
2018-07-06 09:49:00 +0500

    Init

---
+++ DataAccess/TestContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repro
+{
+    public class TestContext : DbContext
+    {
+        public DbSet<TestEntity> TestEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<TestEntity>().Property(x => x.SmallDate).HasColumnType("smalldatetime");
+
+            builder.Entity<TestEntity>().HasData(
+                new TestEntity { Id = new Guid("61242D0B-DDCF-46A2-B853-F50A30ECE25B"), SmallDate = DateTime.UtcNow }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Repro;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+
+    public class TestEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime? SmallDate { get; set; }
+    }
+}
---
+++ Web/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Repro;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        private readonly TestContext _context;
+
+        public TestController(TestContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult AddTestEntity()
+        {
+            var result = _context.TestEntities.Add(new TestEntity
+            {
+                SmallDate = DateTime.UtcNow
+            });
+            _context.SaveChanges();
+
+            return Ok(JsonConvert.SerializeObject(result.Entity));
+        }
+    }
+}
